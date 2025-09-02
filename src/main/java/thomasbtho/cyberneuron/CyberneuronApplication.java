3b7cf4ea0c72987b3fe87d97ab39b71d2c07c5f6
Chasse-Poulet <thomas.bauque@gmail.com>
2025-09-02 11:21:00 +0200

    first commit

---
+++ src/main/java/thomasbtho/cyberneuron/CyberneuronApplication.java
@@ -0,0 +1,13 @@
+package thomasbtho.cyberneuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberneuronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberneuronApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thomasbtho/cyberneuron/CyberneuronApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasbtho.cyberneuron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberneuronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
