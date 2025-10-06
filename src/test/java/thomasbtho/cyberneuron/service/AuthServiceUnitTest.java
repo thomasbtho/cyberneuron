e9cdae134eeade725874b168edb1934d06dab165
Chasse-Poulet <thomas.bauque@gmail.com>
2025-10-06 13:50:04 +0200

    refactor: rename AuthServiceImplTest to AuthServiceUnitTest

--- src/test/java/thomasbtho/cyberneuron/service/AuthServiceUnitTest.java
+++ src/test/java/thomasbtho/cyberneuron/service/AuthServiceUnitTest.java
@@ -25,7 +25,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class AuthServiceImplTest {
+class AuthServiceUnitTest {
 
     @Mock
     private UserRepository userRepository;
